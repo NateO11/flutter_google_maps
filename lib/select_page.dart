07aeaf611f6b5d44294918b577d5c54631146154
NateO11 <ndo1503@gmail.com>
2023-05-07 23:37:18 -0400

    committ

---
+++ lib/select_page.dart
@@ -0,0 +1,16 @@
+// select_page.dart
+import 'package:flutter/material.dart';
+
+class SelectPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select'),
+      ),
+      body: Center(
+        child: Text('Select Page Content'),
+      ),
+    );
+  }
+}
